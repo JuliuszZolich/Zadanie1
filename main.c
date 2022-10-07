0360e94f893ad9f863aba132e20ec781185efd06
student <248673@edu.p.lodz.pl>
2022-10-07 09:21:55 +0200

    test

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    printf("Jakas petla\n");
+    printf("Jakas petla ale od tylu\n");
     for (int i = 0; i < 10; ++i) {
-        printf("\t%i\n",i);
+        printf("\t%i\n ",i);
     }
     return 0;
 }
